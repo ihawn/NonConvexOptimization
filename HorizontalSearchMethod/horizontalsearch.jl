a78f72feccdfea2c6ad7ebe7866326cd35dd70b0
ihawn <ihawn.pl15@gmail.com>
2021-05-22 17:18:51 -0500

    -

--- HorizontalSearchMethod/horizontalsearch.jl
+++ HorizontalSearchMethod/horizontalsearch.jl
@@ -60,10 +60,10 @@ function Grad_Descent(_f, _x, _α, _β, _ϵ, _κ)
     ∇f = Compute_Gradient(_f, _x)
     normGrad = norm(∇f)
 
-    push!(xPlot, _x)
-    push!(yPlot, _f(_x))
-
     val = _f(_x)
+
+    push!(xPlot, _x[1])
+    push!(yPlot, _x[2])
 
     while normGrad > _ϵ
         ∇f = Compute_Gradient(_f, _x)
@@ -74,13 +74,9 @@ function Grad_Descent(_f, _x, _α, _β, _ϵ, _κ)
         _x -= t*∇f
         val = _f(_x)
 
-        push!(xPlot, _x)
-        push!(yPlot, _f(_x))
-
-
+        push!(xPlot, _x[1])
+        push!(yPlot, _x[2])
     end
-    push!(xSol, _x)
-    push!(ySol, _f(_x))
 
     return _x, val
 end
@@ -93,31 +89,14 @@ end
 
 function Search(local_sol, _f, _ℓ, _γ, _η)
 
-    push!(xSearchRight, local_sol[1])
-    push!(ySearchRight, local_sol[2])
-    push!(xSearchLeft, local_sol[1])
-    push!(ySearchLeft, local_sol[2])
-
-    push!(xSearchRight, local_sol[1] + _ℓ)
-    push!(ySearchRight, local_sol[2])
-    push!(xSearchLeft, local_sol[1] - _ℓ)
-    push!(ySearchLeft, local_sol[2])
 
     while _f(local_sol[1] + _ℓ) >= local_sol[2] + _η && _f(local_sol[1] - _ℓ) >= local_sol[2] + _η && _ℓ > _η
         _ℓ *= _γ
-        push!(xSearchRight, local_sol[1] + _ℓ)
-        push!(ySearchRight, local_sol[2])
-        push!(xSearchLeft, local_sol[1] - _ℓ)
-        push!(ySearchLeft, local_sol[2])
     end
 
     if _f(local_sol[1] + _ℓ) < local_sol[2] + _η
-        push!(xSearchRight, local_sol[1] + _ℓ)
-        push!(ySearchRight, _f(local_sol[1] + _ℓ))
         return local_sol[1] + _ℓ, _ℓ
     else
-        push!(xSearchLeft, local_sol[1] - _ℓ)
-        push!(ySearchLeft, _f(local_sol[1] - _ℓ))
         return local_sol[1] - _ℓ, _ℓ
     end
 end
@@ -126,11 +105,11 @@ end
 function Horizontal_Search(_f, _x, _α, _β, _η, _ϵ, _κ, _ℓ, _γ, width, maxIt)
 
     sol = Grad_Descent(_f, _x, _α, _β, _η, _κ)
-    s = Search(sol, _f, _ℓ, _γ, _η)
-    x_prev = s[1]
-    _ℓ = s[2]
+    #s = Search(sol, _f, _ℓ, _γ, _η)
+    #x_prev = s[1]
+    #_ℓ = s[2]
 
-    while abs(norm(_x) - norm(x_prev)) > _η
+#=    while abs(norm(_x) - norm(x_prev)) > _η
 
         s = Search(sol, _f, _ℓ, _γ, _η)
         x_prev = s[1]
@@ -142,48 +121,41 @@ function Horizontal_Search(_f, _x, _α, _β, _η, _ϵ, _κ, _ℓ, _γ, width, ma
         _ℓ = s[2]
     end
 
-    sol = Newton(_f, _x, _ϵ)
+    sol = Newton(_f, _x, _ϵ)=#
 
     return sol
 end
 
 
 
-#f(x) = x^2 + sin(3x^2)
-#f(x) = x^4 + 3x^3 + x^2 + x + sin(3x^4)
-#f(x) = x^4 - x^3 - 50x^2 + 100*sin(30x)
-#f(x) = x^2/20 + 10sin(x) + 5sin(5x)
-f(x) = x^4/20000 - x^3/1000 - x^2/10 + 10sin(x) + 5sin(5x)
-
-x0 = -50.0
+x0 = [2,3]
 ϵ = 1e-8
 η = 1e-2
 α = 0.5
 β = 0.8
-κ = 0.01
+κ = 0.5
 ℓ = 35
 γ = 0.99
 searchWidth = 10
+
 xPlot = []
 yPlot = []
-xSol = []
-ySol = []
-xSearchLeft = []
-ySearchLeft = []
-xSearchRight = []
-ySearchRight = []
-xSolFinal = []
-ySolFinal = []
+solPlot = []
 
 var = x0
 maxIterations = 150
 
+f(x) = x[1]^2 + x[2]^2 + 5*sin(x[1] + x[2])
 
 minimum = Horizontal_Search(f, x0, α, β, η, ϵ, κ, ℓ, γ, searchWidth, maxIterations)
-xlist = range(-50.0, 65.0, length = 1000)
-plot( xlist, f.(xlist), #=xrange = (25,50), yrange = (-115,-40),=# legend = false)
-scatter!(xPlot, yPlot, markersize = 2)
-scatter!(xSol, ySol)
-plot!(xSearchRight, ySearchRight)
-plot!(xSearchLeft, ySearchLeft)
-scatter!(xSolFinal, ySolFinal, color = "Red")
+
+plotf(x,y) = f([x, y])
+_x = -4.0:0.03:4.0
+_y = -4.0:0.03:4.0
+X = repeat(reshape(_x, 1, :), length(_y), 1)
+Y = repeat(_y, 1, length(_x))
+Z = map(plotf, X, Y)
+p1 = Plots.contour(_x,_y, plotf, fill = true)
+plot(p1, legend = false)
+plot!(xPlot, yPlot)
+scatter!(xPlot, yPlot)
