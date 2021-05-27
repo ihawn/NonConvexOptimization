01498036708e542ce5e0a74e9bf032ee1f48d2db
ihawn <ihawn.pl15@gmail.com>
2021-05-26 19:57:07 -0500

    Update basin_hopping.jl

--- BasinHopping/basin_hopping.jl
+++ BasinHopping/basin_hopping.jl
@@ -118,40 +118,64 @@ function Monte_Carlo_Step(old_val, _f, _x, _ℓ, _η, _α, _β, _κ, _T)
 end
 
 
-function Adjust_Step_Length(target_rate, rate, scale_factor, _ℓ)
+function Adjust_Step_Length(target_rate, rate, scale_factor, _ℓ, _ℓ_range,)
 
     if rate > target_rate #stuck in a local min so increase step length
-        _ℓ /= scale_factor
+        _ℓ = min(_ℓ_range[2], ℓ/scale_factor)
     else #random search is all over the place so take smaller steps
-        _ℓ *= scale_factor
+        _ℓ = max(_ℓ_range[1], ℓ*scale_factor)
     end
 
     return _ℓ
 end
 
 
-function Basin_Hopping(_f, _x, _α, _β, _η, _ϵ, _κ, _ℓ, _γ, _ρ, _T, target_rate, width, maxIt)
+function Basin_Hopping(_f, _x, _α, _β, _η, _ϵ, _κ, _ℓ, _ℓ_range, _γ, _T, target_rate, maxIt, stat_thresh)
 
+    static_count = 0
     acceptance = 0
     rejection = 0
+    min_sol = Grad_Descent(_f, _x, _α, _β, _η, _κ)
 
     for i = 1:maxIt
-        println("Iteration ", i)
         sol = Grad_Descent(_f, _x, _α, _β, _η, _κ)
+        val = sol[2]
         accept = Monte_Carlo_Step(sol[2], _f, _x, _ℓ, _η, _α, _β, _κ, _T)
 
         if accept[1]
             _x = accept[2][1]
+            val = accept[2][2]
+
+            if val < min_sol[2] - _η
+                min_sol = accept[2]
+                static_count = 0
+            else
+                static_count += 1
+            end
+
             acceptance += 1
         else
             rejection += 1
+            static_count += 1
+        end
+
+        if static_count >= stat_thresh
+            break
         end
 
         acceptance_rate = acceptance/(acceptance + rejection)
-        _ℓ = Adjust_Step_Length(target_rate, acceptance_rate, γ, _ℓ)
+        _ℓ = Adjust_Step_Length(target_rate, acceptance_rate, γ, _ℓ, _ℓ_range,)
+
+
+        println("\nIteration ", i)
+        println(static_count)
+        println("Step Acceptance Rate = ", 100*acceptance_rate, "%")
+        println("Step size = ", _ℓ)
+        println("x = ", _x)
+        println("Objective = ", val)
     end
 
-    sol = Unconstrained_Newton(_f, _x, _α, _β, _κ, _ϵ, maxIt)
+    sol = Unconstrained_Newton(_f, min_sol[1], _α, _β, _κ, _ϵ, maxIt)
 
     println(sol)
 
@@ -165,12 +189,13 @@ x0 = [-10.0,10.0]
 η = 1e-3
 α = 0.5
 β = 0.8
-κ = 0.1
+κ = 1
 ℓ = 1
-γ = 0.5
+ℓ_range = (0.001, 250)
+γ = 0.99
 ρ = 2
 T = 10
-searchWidth = 10
+static_threshold = 100 #number of iterations that we allow the solution to stay the same. Used as a stopping condition
 target_acc_rate = 0.5
 
 
@@ -184,29 +209,28 @@ finalSolX = []
 finalSolY = []
 
 var = x0
-maxIterations = 5e2
+maxIterations = 5e3
 
 #f(x) = x[1]^2 + x[2]^2 + 7*sin(x[1] + x[2]) + 10*sin(5x[1])
 #f(x) = (x[2] - 0.129*x[1]^2 + 1.6*x[1] - 6)^2 + 6.07*cos(x[1]) + 10
-#f(x) = Rastrigin(x, 2)
-f(x) = Ackley(x)
+f(x) = Rastrigin(x, 2)
+#f(x) = Ackley(x)
 #f(x) = Bukin(x)
 #f(x) = Holder_Table(x)
 #f(x) = Schaffer_N2(x)
 #f(x) = Styblinski_Tang(x,2)
 
-@time minimum = Basin_Hopping(f, x0, α, β, η, ϵ, κ, ℓ, γ, ρ, T, searchWidth, maxIterations)
+@time minimum = Basin_Hopping(f, x0, α, β, η, ϵ, κ, ℓ, ℓ_range, γ, T,
+                            target_acc_rate, maxIterations, static_threshold)
 
 plotf(x,y) = f([x, y])
 _x = -10.0:0.03:10.0
 _y = -10.0:0.03:10.0
-#_x = 0.0:0.03:15.0
-#_y = -5.0:0.03:20.0
 X = repeat(reshape(_x, 1, :), length(_y), 1)
 Y = repeat(_y, 1, length(_x))
 Z = map(plotf, X, Y)
 p1 = Plots.contour(_x,_y, plotf, fill = true)
-plot(p1, legend = false, title = "Global Minimization With Basin Hopping")
+plot(p1, legend = false, #=xrange = (-10,10), yrange = (-10,10),=# title = "Global Minimization With Basin Hopping")
 scatter!(searchX, searchY, markersize = 2.5, color = "blue")
 plot!(xPlot, yPlot, color = "white")
 scatter!(xPlot, yPlot, markersize = 2, color = "red")
